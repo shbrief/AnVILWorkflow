ad80bd22b761735b0ff6ce42a5199393e1a27391
Sehyun Oh <sehyunoh@Sehyuns-MacBook-Air.local>
2023-02-14 13:00:03 -0500

    remove map.rda

--- R/data.R
+++
@@ -1,5 +0,0 @@
-#' @name map
-#' @title The configuration of the workspaces manually curated in this package
-#' @format A data frame containing the details of the workspace configuration
-#' @author Sehyun Oh \email{shbrief@gmail.com}
-"map"
