ac8ae12e396fcb1a7b21e1fa87306ee1caed878c
Sehyun Oh <shbrief@gmail.com>
2024-03-25 15:28:27 -0400

    Update documentation

--- R/getMetaTables.R
+++ R/getMetaTables.R
@@ -1,13 +1,17 @@
 # Returning AnVIL workspace urls
 .wsURL <- function(workspace_namespace, workspace_name) {
-    workspace_url <- paste0("https://app.terra.bio/#workspaces/", 
-                            URLencode(workspace_namespace), 
-                            "/", 
-                            URLencode(workspace_name))
+    workspace_url <- file.path("https://app.terra.bio/#workspaces",
+                               URLencode(workspace_namespace), 
+                               URLencode(workspace_name))
     return(workspace_url)
 }
 
 #' Creates a metadata table of all workspaces
+#' 
+#' Metadata of all the GCP-based workspaces are collected.
+#' 
+#' @import dplyr
+#' @importFrom tidyr unnest
 #'
 #' @export
 getWorkspaces <- function() {
@@ -15,10 +19,11 @@ getWorkspaces <- function() {
     response <- as.list(response)
     dat <- cbind(response$workspace, 
                  accessLevel = response$accessLevel, 
-                 public = response$public)
+                 public = response$public) %>%
+        filter(cloudPlatform == "Gcp") # <<<<<<<<<<<<<<<<<< Only GCP now
     workspaces <- dat %>%
         as_tibble(.name_repair = "universal") %>%
-        unnest(attributes) %>%
+        tidyr::unnest(attributes) %>%
         select(workspace_namespace = namespace,
                workspace_name = name,
                accessLevel = accessLevel,
@@ -44,7 +49,7 @@ getWorkspaces <- function() {
         mutate(idx = row_number(), .before = 1)
     
     workspaces <- as.data.frame(workspaces) %>% 
-        unnest(datatype) %>% 
+        tidyr::unnest(datatype) %>% 
         select(-itemsType) %>% 
         rename(data_type = items)
     
@@ -92,115 +97,96 @@ getWorkflows <- function(allWorkspaces){
 }
 
 
-avsampledata <-function(namespace = avworkspace_namespace(), 
-                        name = avworkspace_name()) {
+#' @importFrom rlang is_scalar_character
+#' 
+.avSampleData <- function(namespace = avworkspace_namespace(), 
+                          name = avworkspace_name()) {
     
-    stopifnot(is_scalar_character(namespace), is_scalar_character(name))
-    focal_cols <- c("COUNTRY",
-                    "COUNTRY_OF_RECRUITMENT",
-                    "LIBRARY_LAYOUT",
-                    "LIBRARY_STRATEGY",
-                    "LIBRARY_SOURCE",
-                    "LIBRARY-1_NAME",
-                    "LIBRARY_PREP_KIT_METHOD",
-                    "LIBRARY_CONSTRUCTION_PROTOCOL", 
-                    "SEQUENCING_PROTOCOL__INSTRUMENT_MANUFACTURER_MODEL",
-                    "ET_PILOT_PLATFORMS", 
-                    "LC_PILOT_PLATFORMS", 
-                    "MAIN_PROJECT_E_PLATFORM", 
-                    "MAIN_PROJECT_LC_PLATFORM", 
-                    "PHASE1_E_PLATFORM",
-                    "PHASE1_LC_PLATFORM",
-                    "PLATFORM", 
-                    "PLATFORM_NAME", 
-                    "INSTRUMENT", 
-                    "INSTRUMENT_MODEL", 
-                    "INSTRUMENT_PLATFORM",
-                    "SEQ_PLATFORM",
-                    "SEQUENCING_PLATFORM",
-                    "SEQUENCING_PLATFORM_MODEL",
-                    "PFB:STUDY_DESCRIPTION",
-                    "DESCRIPTION",
-                    "DISEASE_MORPHOLOGY",
-                    "PHENOTYPE_DESCRIPTION",
-                    "CORONARY_ARTERY_DISEASE",
-                    "SPECIMEN_FROM_ORGANISM__ORGAN", 
-                    "SPECIMEN_FROM_ORGANISM__ORGAN_PART", 
-                    "TISSUE_SOURCE",
-                    "SPECIES", 
-                    "DONOR_ORGANISM__GENUS_SPECIES", 
-                    "GENOTYPHI_SPECIES", 
-                    "SONNEITYPING_SPECIES", 
-                    "ANI_TOP_SPECIES_MATCH",
-                    "AGE_AT_INDEX",
-                    "AGE", 
-                    "PFB:AGE_AT_INDEX", 
-                    "AGE_RANGE", 
-                    "DONOR_ORGANISM__ORGANISM_AGE",
-                    "COHORT", 
-                    "GENDER",
-                    "SEX",
-                    "SEX_CALL",
-                    "REPORTED_GENDER", 
-                    "REPORTED_SEX", 
-                    "PFB:ANNOTATED_SEX", 
-                    "HAS_PHENOTYPIC_SEX",
-                    "DONOR_ORGANISM__SEX",
-                    "POPULATION", 
-                    "POPULATION_LABEL", 
-                    "SUBPOPULATION", 
-                    "SUPERPOPULATION",
-                    "PFB:POPULATION",
-                    "POPULATION_DESCRIPTION", 
-                    "SUPER_POPULATION_DESCRIPTION",
-                    "POPULATION_ID")
+    ## Sanity check
+    stopifnot(rlang::is_scalar_character(namespace), 
+              rlang::is_scalar_character(name))
     
-    avdata <- avtables(namespace, name)
-    avdata[, focal_cols] <- NA
+    ## Columns/Attributes for curation
+    dir <- system.file("extdata", package = "AnVILWorkflow")
+    curated_cols <- readLines(file.path(dir, "curated_cols.txt"))
     
-    for (i in seq_len(nrow(avdata))) {
-        avtb <- avtable(as.character(avdata$table[i]), 
-                        namespace, 
-                        name)
+    res <- avtables(namespace, name) # a table of metadata tables
+    set_ind <- grep("_set$", res$table) # remove metadata tables for set
+    res <- res[-set_ind,]
+    res[, curated_cols] <- NA # add the curated_cols to the master table
+    
+    for (i in seq_len(nrow(res))) {
+        avtb <- avtable(as.character(res$table[i]), namespace, name)
         colnames(avtb) <- toupper(colnames(avtb))
         
-        columns <- intersect(focal_cols, colnames(avtb))
-        for (column in columns){
-            vals <- paste(unlist(unique(avtb[, column])), collapse = "<;>")
-            avdata[i, column] <- vals
+        ## If the metadata table contains any of the to-be-curated columns
+        columns <- intersect(curated_cols, colnames(avtb))
+        for (column in columns) {
+            
+            ## Gender/Sex Curation: Convert to descriptive values
+            sex_cols <- c("SEX_CALL", "SEX", "GENDER")
+            if (column %in% sex_cols) {
+                tb_with_sex <- which(!is.na(avtb[column]))
+                sex_vals <- unlist(avtb[, column]) %>%
+                    gsub("^M$|1|^male$", "Male", .) %>%
+                    gsub("^F$|2|^female$", "Female", .) %>% 
+                    ifelse(is.na(.), "NA", .) %>%
+                    table()
+                vals <- .tableToString(sex_vals)
+                res[i, column] <- vals
+            } else {
+                ## Save all the unique values from the to-be-curated columns
+                vals <- paste(unique(unlist(avtb[, column])), collapse = "<;>")
+                res[i, column] <- vals
+            }
         }
     }
-    
-    # Initialize age range columns
-    avdata$age_min <- NA
-    avdata$age_max <- NA
-    
-    # Identify min and max value for each table
-    for (col in c("AGE", "PFB:AGE_AT_INDEX", "AGE_RANGE", "DONOR_ORGANISM__ORGANISM_AGE")){
-      for (table in which(!is.na(avdata[,col]))){
-        ages <- list()
-        for (i in unlist(strsplit(as.character(avdata[table,col]), split="<;>|-| year"))){
-          if (i!="NA"){
-            ages <- append(ages, as.numeric(i))
-          }
-        }
-        ages <- data.frame(all_age = unlist(ages))
-        avdata$age_min[table] <- min(ages$all_age)
-        avdata$age_max[table] <- max(ages$all_age)
-      }
-    }
-    
-    # Gender/Sex Curation: Convert to descriptive values
-    avdata$SEX_CALL[which(avdata$SEX_CALL=="F<;>M<;>UKN")] <- "Female<;>Male<;>Unknown"
-    avdata$SEX[which(avdata$SEX=="1<;>2" | avdata$SEX=="M<;>F")] <- "Male<;>Female"
-    avdata$GENDER[which(avdata$GENDER=="1<;>2")] <- "Male<;>Female"
-    
-    # Disease Curation: Convert to descriptive values
-    avdata$CORONARY_ARTERY_DISEASE[which(!is.na(avdata$CORONARY_ARTERY_DISEASE))] <- "Coronary Artery Disease<;>No"
-    
-    avdata$workspace_key <- paste(namespace, name, sep = ":") 
-    return(avdata)
+    return(res)
 }
+
+
+#' Summary table into a single string where the column name and the value
+#' is separated by `sep` and columns are separated by `delim`
+#' 
+#' @param tb A table. Output from the `table` function. 
+#' @param sep A delimiter to separate the column name and value
+#' @param delim A delimiter to separate columns
+#' 
+.tableToString <- function(tb, sep = ":", delim = ";") {
+    attr <- names(tb)
+    attr_num <- as.character(tb)
+    res <- paste(attr, attr_num, sep = ":") %>% 
+        paste(., collapse = ";")
+    return(res)
+}
+    
+#' Apply to the returned value from `.avSampleData` function
+#' 
+#' @importFrom rlang is_scalar_character
+#' @param sampleData A data frame. 
+#' 
+.getAgeMinMax <- function(sampleData) {
+    
+    ## Initialize age range columns
+    sampleData$AGE_MIN <- NA
+    sampleData$AGE_MAX <- NA
+    
+    ## Identify min and max ages
+    age_cols <- c("AGE", "PFB:AGE_AT_INDEX", "AGE_RANGE", 
+                  "DONOR_ORGANISM__ORGANISM_AGE")
+    
+    for (age_col in age_cols) {
+        tb_with_age <- which(!is.na(sampleData[age_col]))
+        for (i in tb_with_age) {
+            ages <- strsplit(as.character(sampleData[i, age_col]), 
+                             split = "<;>|-| year") %>% unlist
+            ages <- ages[ages != "NA"] %>% as.numeric
+            sampleData$AGE_MIN[i] <- min(ages)
+            sampleData$AGE_MAX[i] <- max(ages)
+        }
+    }
+    return(sampleData)
+}  
 
 
 #' Creates a metadata table of data from all workspaces provided
@@ -217,18 +203,19 @@ getData <- function(allWorkspaces){
     data <- data.frame()
     for (i in seq_len(nrow(allWorkspaces))) {
         msg <- paste(i, "out of", nrow(allWorkspaces))
-        print(i)
+        print(msg)
         
         workspace_namespace <- allWorkspaces$workspace_namespace[i]
         workspace_name <- allWorkspaces$workspace_name[i]
-        res <- avsampledata(namespace = workspace_namespace,
-                            name = workspace_name) 
+        res <- .avSampleData(namespace = workspace_namespace,
+                             name = workspace_name) %>% 
+            .getAgeMinMax(.)
         
         res$workspace_key <- allWorkspaces$workspace_key[i]
         data <- plyr::rbind.fill(data, res)
     }
     
-    ## Add the name_key
-    data$name_key <- paste(data$name, data$workspace_key, sep = ":")
+    # ## Add the name_key
+    # data$name_key <- paste(data$name, data$workspace_key, sep = ":")
     return(data)
 }
